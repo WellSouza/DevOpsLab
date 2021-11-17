a014c0026164f3d8f63db4e1f4fb25519a05fa1f
WellSouza <33166767+WellSouza@users.noreply.github.com>
2021-11-17 19:54:13 -0300

    Update app.py
    Mudando a MSG!

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def pagina_inicial():
-    return "Hoje a Dama da SOFRENCIA MORREU"
+    return "Hoje a Dama da SOFRENCIA MORREU - Mas hoje, ela vive em nossos corações!!"
 
 if __name__ == '__main__':
     app.run()
